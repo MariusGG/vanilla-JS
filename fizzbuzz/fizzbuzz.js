c605d20f21f3863eedf9ae2e5454bffa903f9214
Marius Guei <marius.g.guei@gmail.com>
2019-03-11 15:09:09 +0000

    return Fizz if number is divisible 3 and equal 0

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,1 +1,9 @@
 
+module.exports = function fizzbuzz(num) {
+  if(num % 3 == 0){
+    return "Fizz"
+  }else {
+    return num;
+  }
+  
+};
--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,5 +1,13 @@
 const fizzbuzz = require('./fizzbuzz');
 
-test('to return toBeUndefined', () => {
-  expect(fizzbuzz()).toBeUndefined();
+// test('to return toBeUndefined', () => {
+//   expect(fizzbuzz()).toBeUndefined();
+// })
+
+test('when given 1 return 1', () => {
+  expect(fizzbuzz(1)).toEqual(1);
+})
+
+test('when given 3 return Fizz', () => {
+  expect(fizzbuzz(3)).toEqual('Fizz')
 })
