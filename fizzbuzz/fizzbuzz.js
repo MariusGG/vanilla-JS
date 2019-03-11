5b71bdbfc1fb7d0f1ddaa5d34c88eeae77e9655f
Marius Guei <marius.g.guei@gmail.com>
2019-03-11 15:16:21 +0000

    passing Buzz test

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,9 +1,10 @@
 
 module.exports = function fizzbuzz(num) {
-  if(num % 3 == 0){
+  if(num % 3 === 0){
     return "Fizz"
+  }else if (num % 5 === 0 ) {
+    return "Buzz"
   }else {
-    return num;
+    return num
   }
-  
 };
